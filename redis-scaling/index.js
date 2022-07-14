8ef0cec66b71926d3879805698ebcbf5b2bb9ad0
PKiran <kiranparajuli589@gmail.com>
2022-07-15 00:21:06 +0545

    Websocket server with rever proxy LB and redis
    Signed-off-by: PKiran <kiranparajuli589@gmail.com>

--- redis-scaling/index.js
+++ redis-scaling/index.js
@@ -1,67 +1,78 @@
-const http = require("http")
-const {createClient} = require("redis")
-const WebSocketServer = require("websocket").server
+import http from "http";
+import ws from "websocket"
+import redis from "redis";
+
+const APP_ID = process.env.APP_ID;
+
+const connections = [];
+const WebSocketServer = ws.server
+const CHANNEL_LIVE_CHAT = "livechat"
+
+const subscriber = redis.createClient({
+  port: 6379,
+  host: 'redis'
+});
+
+const publisher = redis.createClient({
+  port: 6379,
+  host: 'redis'
+});
 
 
-const APP_ID = process.env.APP_ID
-const LIVE_CHAT_CHANNEL = 'livechat'
+subscriber.on("subscribe", function (channel, count) {
+  const subscriptionMessage = `Server ${APP_ID} subscribed successfully to channel ${channel}`;
+  publisher.publish(CHANNEL_LIVE_CHAT, subscriptionMessage);
+});
 
-let connectionPool = []
-
-const subscriber = createClient({
-  url: "http://localhost:6379"
-})
-const publisher = createClient({
-  url: "http://localhost:6379"
-})
-
-subscriber.on("subscribe", (channel, count) => {
-  const subscriptionMessage = `Server "${APP_ID}" has subscribed successfully to the channel "${LIVE_CHAT_CHANNEL}"`
-  console.info(subscriptionMessage)
-  publisher.publish(LIVE_CHAT_CHANNEL, subscriptionMessage).catch(err => console.error(err))
-})
-
-subscriber.on("message", (channel, message) => {
-  // when we receive a message we want to send it to all the subscribed clients
+subscriber.on("message", function (channel, message) {
   try {
-    console.info(`MESSAGE::Channel "${channel}": ${message}`)
-    connectionPool.forEach(connection => {
-      connection.send(`APP_ID: ${APP_ID} - ${message}`)
-    })
-  } catch (e) {
-    console.error(e)
+    //when we receive a message I want to send it to all the clients
+    console.info(`R_INCOMING_MESSAGE::${APP_ID}:: ${message}`);
+    connections.forEach(c => c.send(APP_ID + ":" + message))
+
+  } catch (err) {
+    console.error(err)
   }
-})
+});
 
-// join to a channel
-subscriber.subscribe(LIVE_CHAT_CHANNEL).catch(err => console.error(err))
 
-// create a raw HTTP server
-// this will help us create the TCP connection
-// the TCP conn. will then pass to the websocket to do the job
-const httpServer = http.createServer()
+subscriber.subscribe(CHANNEL_LIVE_CHAT);
 
-// pass the httpServer object to the WebSocketServer constructor,
-// this should override the default HTTP server req/res behavior
+
+//create a raw http server (this will help us create the TCP which will then pass to the websocket to do the job)
+const httpserver = http.createServer()
+
+//pass the httpserver object to the WebSocketServer library to do all the job, this class will override the req/res 
 const websocket = new WebSocketServer({
-  "httpServer": httpServer
+  "httpServer": httpserver
 })
 
-httpServer.listen(8080, () => {
-  console.info(`Server ${APP_ID} listening at 8080`)
-})
 
-websocket.on("request", (request) => {
+httpserver.listen(8080, () => console.info("Server is listening on port 8080."))
+
+//when a legit websocket request comes listen to it and get the connection .. once you get a connection thats it! 
+websocket.on("request", request => {
   const connection = request.accept(null, request.origin)
-  connection.on("open", () => console.log("Opened!"))
-  connection.on("close", () => console.log("CLOSED!"))
-  connection.on("message", (message) => {
-    console.info(`MESSAGE::${APP_ID}: ${message.utf8Data}`)
-    // whenever we get a new message we publish it to the susbcribed channel
-    // so that other clients can receive it too
-    publisher.publish(LIVE_CHAT_CHANNEL, message.utf8Data)
+  connection.on("open", () => {
+    console.log("Opened")
+  })
+  connection.on("close", () => console.log("CLOSED!!!"))
+  connection.on("message", message => {
+    console.log(`WS_INCOMING_MESSAGE::${APP_ID}:: ${message.utf8Data}`)
+    // publish the message to the redis channel
+    publisher.publish(CHANNEL_LIVE_CHAT, message.utf8Data)
   })
 
-  setTimeout(() => connection.send(`Hello from the server ${APP_ID}.`), 1000)
-  connectionPool.push(connection)
+  // periodically send a message to the client
+    setInterval(() => {
+        connection.send(`Hello client. I am Server ${APP_ID}`)
+    }, 5000)
+
+  // periodicall publish a message to the redis channel
+    setInterval(() => {
+        publisher.publish(CHANNEL_LIVE_CHAT, `Hello client. I am Server ${APP_ID}`)
+    }, 5000)
+
+
+  connections.push(connection)
 })
